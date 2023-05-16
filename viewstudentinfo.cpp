ff535d8ffa46aa8f94502fd3c00b004cf1d15256
kagura <me@lolicon.cyou>
2023-05-16 18:39:09 +0800

    a

--- viewstudentinfo.cpp
+++ viewstudentinfo.cpp
@@ -5,9 +5,10 @@ ViewStudentInfo::ViewStudentInfo(Admin* a,QWidget *parent) :
     QWidget(parent),
     ui(new Ui::ViewStudentInfo)
 {
+    //Init
     admin = a;
     ui->setupUi(this);
-<<<<<<< HEAD
+    //init UI
     ui->Qname->setStyleSheet("QPushButton{font: 25 14pt '微软雅黑 regular';color: rgb(0,0,0);background-color: rgb(255,248,220);"
                                   "border: 2px groove gray;border-radius:15px;padding:2px 4px;border-style: outset;}"
                                   "QPushButton:hover{background-color: rgb(22,218,208);}"//hover
@@ -23,9 +24,6 @@ ViewStudentInfo::ViewStudentInfo(Admin* a,QWidget *parent) :
                                   "QPushButton:hover{background-color: rgb(22,218,208);}"//hover
                                   "QPushButton:pressed{background-color: rgb(17,171,164);}"//pressed
                                   "border:2px solid rgb(20,196,188);");//边框粗细-颜色-圆角设置
-=======
-
->>>>>>> f8b383be4f9aeb4015e175bc7f077ac91e45c40f
 
     this->show();
 }
